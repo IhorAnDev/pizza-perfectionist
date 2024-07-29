3a10b200f95045fe805a51c3e885fe0220f999d9
ihorandev <ihorandev@gmail.com>
2024-07-29 08:23:30 +0200

    init commit

---
+++ components/shared/index.ts
@@ -0,0 +1,5 @@
+import { Categories } from "./categories";
+export { Container } from "./container";
+export { Header } from "./header";
+export { Title } from "./title";
+export { Categories } from "./categories";
